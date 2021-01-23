46c7f1c676a3b722cccc30b79dffae9145d1586e
Bereket Semagn <semagnbereket@gmail.com>
2021-01-22 21:19:56 -0500

    new plugin fixes (+ bugs)

--- build/code.js
+++ build/code.js
@@ -1,153 +1,42 @@
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
+
+
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__, { width: 300, height: 300 });
+//function to convert hex color code to RGB because figma takes fill input as RGB
+function hex2rgb(hex) {
+    return [Number('0x' + hex[1] + hex[2]) | 0, Number('0x' + hex[3] + hex[4]) | 0, Number('0x' + hex[5] + hex[6]) | 0];
+}
+// Calls to "parent.postMessage" from within the HTML page will trigger this
+// callback. The callback will be passed the "pluginMessage" property of the
+// posted message.
+figma.ui.onmessage = msg => {
+    if (msg.type === 'create-palette') {
+        const nodes = [];
+        //create frame
+        const colorsFrame = figma.createFrame();
+        //set height and width with 24px padding
+        const frameHeight = 148;
+        const frameWidth = (msg.colorsList.length * 124) + 24;
+        colorsFrame.resizeWithoutConstraints(frameWidth, frameHeight);
+        //set frame name
+        colorsFrame.name = msg.name + " Colors";
+        colorsFrame.x = figma.viewport.center.x;
+        colorsFrame.y = figma.viewport.center.y;
+        //loop to create color rectangles
+        for (let i = 0; i < msg.colorsList.length; i++) {
+            const rect = figma.createRectangle();
+            rect.y = 24;
+            rect.x = 24 + (i * 124);
+            let rgbColor = hex2rgb(msg.colorsList[i]);
+            rect.name = msg.name + "-" + i;
+            rect.fills = [{ type: 'SOLID', color: { r: rgbColor[0] / 255, g: rgbColor[1] / 255, b: rgbColor[2] / 255 } }];
+            figma.currentPage.appendChild(rect);
+            colorsFrame.appendChild(rect);
+            nodes.push(rect);
+        }
+        colorsFrame.setRelaunchData({ rerun: 'Add another palette' });
+        figma.viewport.scrollAndZoomIntoView(nodes);
+    }
+    figma.closePlugin();
 };
-let isText = filterLayers(figma.currentPage.selection, i => i.type == "TEXT");
-function generateDataKey(length) {
-    let result = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-function generatePasswordKey(length) {
-    let result = "";
-    const characters = "`~!@#$%^&*()-_=+[{]}\|;:',<.>/?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-function generateCertificateKey(length) {
-    let result = "";
-    const characters = "/+=_-\ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    const charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-function chunk(str, n) {
-    var value = [];
-    var i;
-    var len;
-    for (i = 0, len = str.length; i < len; i += n) {
-        value.push(str.substr(i, n));
-    }
-    return value;
-}
-function generateAddressKey(length) {
-    let result = "";
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    const charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-function generateIPKey(length) {
-    let result = "";
-    const symbols = "0123456789";
-    const symbolsLength = symbols.length;
-    for (var i = 0; i < length; i++) {
-        result += symbols.charAt(Math.floor(Math.random() * symbolsLength));
-    }
-    return result;
-}
-function generateInitialsKey(length) {
-    let result = "";
-    const symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const symbolsLength = symbols.length;
-    for (var i = 0; i < length; i++) {
-        result += symbols.charAt(Math.floor(Math.random() * symbolsLength));
-    }
-    return result;
-}
-figma.showUI(__html__, {
-    width: 320,
-    height: 640
-});
-if (!isText.length)
-    figma.closePlugin("Please select a text layer to use the plugin.");
-figma.ui.postMessage({});
-figma.ui.onmessage = (msg) => __awaiter(this, void 0, void 0, function* () {
-    console.log(msg);
-    if (msg.type == "generate-data-key") {
-        console.log(isText);
-        for (const n of isText) {
-            yield figma.loadFontAsync(n.fontName);
-            if (n.type === "TEXT" && n.characters) {
-                n.characters = generateDataKey(msg.value);
-            }
-        }
-    }
-    else if (msg.type == "generate-password-key") {
-        for (const n of isText) {
-            yield figma.loadFontAsync(n.fontName);
-            if (n.type === "TEXT" && n.characters) {
-                n.characters = generatePasswordKey(msg.value);
-            }
-        }
-    }
-    else if (msg.type == "generate-certificate-key") {
-        for (const n of isText) {
-            yield figma.loadFontAsync(n.fontName);
-            if (n.type === "TEXT" && n.characters) {
-                n.characters = generateCertificateKey(msg.value);
-            }
-        }
-    }
-    else if (msg.type == "generate-address-key") {
-        for (const n of isText) {
-            yield figma.loadFontAsync(n.fontName);
-            if (n.type === "TEXT" && n.characters) {
-                n.characters = chunk(generateAddressKey(msg.value), 2).join(':');
-            }
-        }
-    }
-    else if (msg.type == "generate-ip-key") {
-        for (const n of isText) {
-            yield figma.loadFontAsync(n.fontName);
-            if (n.type === "TEXT" && n.characters) {
-                n.characters = generateIPKey(msg.valueOne) + "." + generateIPKey(msg.valueTwo) + "." + generateIPKey(msg.valueThree) + "." + generateIPKey(msg.valueFour);
-            }
-        }
-    }
-    else if (msg.type == "generate-initials-key") {
-        for (const n of isText) {
-            yield figma.loadFontAsync(n.fontName);
-            if (n.type === "TEXT" && n.characters) {
-                n.characters = generateInitialsKey(msg.value);
-            }
-        }
-    }
-    else {
-        figma.closePlugin();
-    }
-});
-function filterLayers(data, predicate) {
-    return !!!data
-        ? null
-        : data.reduce((list, entry) => {
-            let clone = null;
-            if (predicate(entry)) {
-                clone = entry;
-                list.push(clone);
-            }
-            else if (entry.children != null) {
-                let children = filterLayers(entry.children, predicate);
-                if (children.length > 0) {
-                    list.push(...children);
-                }
-            }
-            return list;
-        }, []);
-}
+
