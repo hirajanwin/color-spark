cff8fae5074c266be2dd04ed69e570d5338ebd1b
Bereket Semagn <semagnbereket@gmail.com>
2021-01-23 12:14:19 -0500

    updated styling + importing

--- build/code.js
+++ build/code.js
@@ -1,7 +1,7 @@
 
 
 // This shows the HTML page in "ui.html".
-figma.showUI(__html__, { width: 300, height: 300 });
+figma.showUI(__html__, { width: 440, height: 440 });
 //function to convert hex color code to RGB because figma takes fill input as RGB
 function hex2rgb(hex) {
     return [Number('0x' + hex[1] + hex[2]) | 0, Number('0x' + hex[3] + hex[4]) | 0, Number('0x' + hex[5] + hex[6]) | 0];
@@ -15,13 +15,15 @@ figma.ui.onmessage = msg => {
         //create frame
         const colorsFrame = figma.createFrame();
         //set height and width with 24px padding
-        const frameHeight = 148;
+        const frameHeight = 150;
         const frameWidth = (msg.colorsList.length * 124) + 24;
         colorsFrame.resizeWithoutConstraints(frameWidth, frameHeight);
         //set frame name
         colorsFrame.name = msg.name + " Colors";
         colorsFrame.x = figma.viewport.center.x;
         colorsFrame.y = figma.viewport.center.y;
+        figma.notify(`👍 Successfully imported ${msg.name}`)
+
         //loop to create color rectangles
         for (let i = 0; i < msg.colorsList.length; i++) {
             const rect = figma.createRectangle();
@@ -37,6 +39,8 @@ figma.ui.onmessage = msg => {
         colorsFrame.setRelaunchData({ rerun: 'Add another palette' });
         figma.viewport.scrollAndZoomIntoView(nodes);
     }
+
+    
     figma.closePlugin();
 };
 
